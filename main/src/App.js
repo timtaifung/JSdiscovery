fb8c83367bb8e79101dc759513a939c251a67a37
timo <timothynyan@gmail.com>
2022-12-28 12:31:03 +0800

    main header

--- main/src/App.js
+++ main/src/App.js
@@ -4,21 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row App-header">
+        <div className="col-12">
+          <h1>Digital Playground</h1>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
